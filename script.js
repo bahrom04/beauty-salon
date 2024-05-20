d707b6e8afa8b82b6defb27c777cc5f50c4bba4d
bahrom <handkgroup@handk-inc.co.jp>
2024-05-20 12:00:16 +0500

    feat: resourses_1 update map

--- script.js
+++
@@ -1,7 +0,0 @@
-const burger = document.querySelector(".burger");
-const offScreenMenu = document.querySelector(".off_screen__menu");
-
-burger.addEventListener("click", () => {
-  burger.classList.toggle("active");
-  offScreenMenu.classList.toggle("active");
-});
